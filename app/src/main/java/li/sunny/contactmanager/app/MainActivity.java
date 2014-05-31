dc388927668e5e2965a777dd88d86da3e5371617
Sunny Li <LDYsunny@hotmail.com>
2014-05-31 00:32:21 -0400

    add contacts

--- app/src/main/java/li/sunny/contactmanager/app/MainActivity.java
+++ app/src/main/java/li/sunny/contactmanager/app/MainActivity.java
@@ -7,15 +7,22 @@ import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TabHost;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
 
     EditText nameTxt, phoneTxt, emailTxt, addressTxt;
+    List<Contact> Contacts = new ArrayList<Contact>() ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +51,8 @@ public class MainActivity extends ActionBarActivity {
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Your Contact has been created! NOT.", Toast.LENGTH_SHORT).show();
+                addContact(nameTxt.getText().toString(), phoneTxt.getText().toString(), emailTxt.getText().toString(), addressTxt.getText().toString());
+                Toast.makeText(getApplicationContext(), nameTxt.getText().toString() + " has been added to your Contacts!", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -66,6 +74,34 @@ public class MainActivity extends ActionBarActivity {
         });
     }
 
+    private void addContact(String name, String phone, String email, String address) {
+        Contacts.add(new Contact(name, phone, email, address));
+    }
+
+    private class ContactListAdapter extends ArrayAdapter<Contact> {
+        public ContactListAdapter() {
+            super (MainActivity.this, R.layout.listview_item, Contacts);
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup parent) {
+            if (view == null)
+                view = getLayoutInflater().inflate(R.layout.listview_item, parent, false);
+
+            Contact currentContact = Contacts.get(position);
+
+            TextView name = (TextView) view.findViewById(R.id.contactName);
+            name.setText(currentContact.getName());
+            TextView phone = (TextView) view.findViewById(R.id.phoneNumber);
+            phone.setText(currentContact.getPhone());
+            TextView email = (TextView) view.findViewById(R.id.emailAddress);
+            email.setText(currentContact.getEmail());
+            TextView address = (TextView) view.findViewById(R.id.cAddress);
+            address.setText(currentContact.getAddress());
+
+            return view;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
